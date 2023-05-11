edb1062dd71aeaa7393c19f52384507fadacf949
beyzagoknl <beyzagoknl@gmail.com>
2023-05-11 11:17:00 +0200

    budget increase allowed

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
+
 const Budget = () => {
   const { budget } = useContext(AppContext);
+  const [cost, setCost] = useState("");
+
   return (
     <div className="alert alert-secondary">
-      <span>Budget: £{budget}</span>
+      <span>Budget: </span>
+
+      <input
+        required="required"
+        type="number"
+        id="cost"
+        value={cost}
+        style={{ marginLeft: "2rem", size: 10 }}
+        onChange={(event) => setCost(event.target.value)}
+      ></input>
     </div>
   );
 };
